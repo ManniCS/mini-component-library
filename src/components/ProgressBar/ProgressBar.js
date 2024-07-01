9c675b57361374bf839079c1c25fbe5a4e6b0750
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-07-01 18:27:19 -0400

    Pixel perfect

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -7,49 +7,62 @@ import VisuallyHidden from '../VisuallyHidden';
 
 const SIZES = { 
   small: { 
-    height: '8px',
-    padding: '0px'
+    height: 8,
+    padding: 0,
+    radius: 4,
   }, 
   medium: { 
-    height: '12px',
-    padding: '0px',
+    height: 12,
+    padding: 0,
+    radius: 4,
   }, 
   large: { 
-    height: '24px',
-    padding: 4 + 'px',
+    height: 16,
+    padding: 4,
+    radius: 8,
   }
 }
 
 const ProgressBar = ({ value, size }) => {
-  return <ProgressWrapper style={SIZES[size]} role="progressbar" aria-valuenow={value} aria-labelledby="progressbarlabel">
-    <VisuallyHidden id="progressbarlabel">Progress bar</VisuallyHidden>
-    <BarWrapper>
-      <Bar style={{'--progressPercent': value + '%'}}/>
-    </BarWrapper>
-  </ProgressWrapper>;
+  const styles = SIZES[size];
+
+  return (
+    <ProgressWrapper 
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }} role="progressbar" aria-valuenow={value} aria-labelledby="progressbarlabel"
+    >
+      <VisuallyHidden id="progressbarlabel">Progress bar</VisuallyHidden>
+      <BarWrapper>
+        <Bar 
+          style={{
+            '--progressPercent': value + '%',
+            '--height': styles.height + 'px',
+          }}
+        />
+      </BarWrapper>
+    </ProgressWrapper>
+  );
 };
 
 const ProgressWrapper = styled.div`
-  width: 370px;
-  height: var(--height);
-  border-radius: 4px;
   background-color: ${COLORS.transparentGray15};
   box-shadow: 0 2px 4px ${COLORS.transparentGray35} inset;
+  border-radius: 4px;
   padding: var(--padding);
 `
 
 const BarWrapper = styled.div`
-  overflow: hidden;
   border-radius: 4px;
-  background: none;
-  height: 100%;
+  overflow: hidden;
 `
 
 const Bar = styled.div`
+  width: var(--progressPercent); 
+  height: var(--height);
   background-color: ${COLORS.primary};
   border-radius: 4px 0 0 4px;
-  width: var(--progressPercent); 
-  height: 100%;
 `
 
 export default ProgressBar;
