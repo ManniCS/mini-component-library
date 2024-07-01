184a2ca5ce0c4ad972360b8ed880d89dd082a621
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-07-01 18:13:01 -0400

    Divs

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,34 +5,51 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+const SIZES = { 
+  small: { 
+    height: '8px',
+    padding: '0px'
+  }, 
+  medium: { 
+    height: '12px',
+    padding: '0px',
+  }, 
+  large: { 
+    height: '24px',
+    padding: 4 + 'px',
+  }
+}
+
 const ProgressBar = ({ value, size }) => {
-  return <Wrapper value={value} max={100}></Wrapper>;
+  return <ProgressWrapper style={SIZES[size]} role="progressbar" aria-valuenow={value} aria-labelledby="progressbarlabel">
+    <VisuallyHidden id="progressbarlabel">Progress bar</VisuallyHidden>
+    <BarWrapper>
+      <Bar style={{'--progressPercent': value + '%'}}/>
+    </BarWrapper>
+  </ProgressWrapper>;
 };
 
-const Wrapper = styled.progress`
-  /* Reset default appearance */
-  -webkit-appearance: none;
-     -moz-appearance: none;
-          appearance: none;
+const ProgressWrapper = styled.div`
+  width: 370px;
+  height: var(--height);
+  border-radius: 4px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: 0 2px 4px ${COLORS.transparentGray35} inset;
+  padding: var(--padding);
+`
 
-  &::-webkit-progress-inner-element {
-    width: 370px;
-    height: 30px;;
-    border-radius: 4px;
-    background-color: ${COLORS.transparentGray15};
-    box-shadow: 0 2px 4px ${COLORS.transparentGray35} inset;
-    overflow: hidden;
-    padding: 10px;
-  } 
-  &::-webkit-progress-bar { 
-    overflow: hidden;
-    border-radius: 4px;
-    background: none;
-  }
-  &::-webkit-progress-value {
-    background-color: ${COLORS.primary};
-    border-radius: 4px 0 0 4px;
-  } 
+const BarWrapper = styled.div`
+  overflow: hidden;
+  border-radius: 4px;
+  background: none;
+  height: 100%;
+`
+
+const Bar = styled.div`
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+  width: var(--progressPercent); 
+  height: 100%;
 `
 
 export default ProgressBar;
