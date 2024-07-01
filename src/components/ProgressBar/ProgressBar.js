7c9421122a5918b9ea8c95141050b491b9049bea
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-07-01 12:42:42 -0400

    ProgressBar with CSS: Chrome

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,33 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return <Wrapper value={value} max={100}></Wrapper>;
 };
+
+const Wrapper = styled.progress`
+  /* Reset default appearance */
+  -webkit-appearance: none;
+     -moz-appearance: none;
+          appearance: none;
+
+  &::-webkit-progress-inner-element {
+    width: 370px;
+    height: 30px;;
+    border-radius: 4px;
+    background-color: ${COLORS.transparentGray15};
+    box-shadow: 0 2px 4px ${COLORS.transparentGray35} inset;
+    overflow: hidden;
+    padding: 10px;
+  } 
+  &::-webkit-progress-bar { 
+    overflow: hidden;
+    border-radius: 4px;
+    background: none;
+  }
+  &::-webkit-progress-value {
+    background-color: ${COLORS.primary};
+    border-radius: 4px 0 0 4px;
+  } 
+`
 
 export default ProgressBar;
