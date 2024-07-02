d1ac3e278beaaa0d84c6ea42bc7f4008c399e79d
Manuele Cavalli-Sforza <manuele.cavallisforza@gmail.com>
2024-07-02 12:55:55 -0400

    IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: { 
+    fontSize: 14,
+    iconSize: 16,
+    strokeWidth: 1,
+    height: 24,
+  },
+  large: { 
+    fontSize: 18,
+    iconSize: 24,
+    strokeWidth: 2,
+    height: 36,
+  }
+};
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,75 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
-};
+  const styles = STYLES[size];
+
+  return (
+    <Wrapper>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Input 
+        placeholder={placeholder}
+        style={{
+          '--width': width + 'px',
+          '--height': styles.height / 16 + 'rem', 
+          '--strokeWidth': styles.strokeWidth + 'px',
+          '--fontSize': styles.fontSize / 16 + 'rem',
+          '--leftPadding': styles.height + 'px',
+        }}
+      ></Input>
+      <IconWrapper
+        style={{
+          '--size': styles.iconSize + 'px'
+        }} 
+      >
+        <Icon 
+          id={icon}
+          size={styles.iconSize}
+          strokeWidth={styles.strokeWidth}
+        />
+      </IconWrapper>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.label`
+  position: relative;
+
+  color: ${COLORS.gray700};
+
+  &:hover { 
+    color: ${COLORS.black};
+  }
+`
+
+const Input = styled.input`
+  width: var(--width);
+  height: var(--height);
+
+  padding-left: var(--leftPadding);
+  font: var(--fontSize) 'Roboto', sans-serif;
+
+  border: none;
+  border-bottom: var(--strokeWidth) solid ${COLORS.black};
+
+  &:focus { 
+    outline-offset: 4px;
+  }
+
+  font-weight: 700;
+  color: inherit;
+
+  ::placeholder { 
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`
 
 export default IconInput;
